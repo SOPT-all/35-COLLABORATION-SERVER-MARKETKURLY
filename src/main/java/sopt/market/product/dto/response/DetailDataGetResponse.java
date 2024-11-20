d96fb8bcebf82456bfa5e66ed42c8f3e0e91ab3d
sae2say <lollol0617@gmail.com>
2024-11-21 01:40:57 +0900

    [FIX] 단일 반환값을 가지도록 수정

--- src/main/java/sopt/market/product/dto/response/DetailDataGetResponse.java
+++ src/main/java/sopt/market/product/dto/response/DetailDataGetResponse.java
@@ -4,58 +4,44 @@ import sopt.market.product.entity.CategoryScope;
 import sopt.market.product.entity.DelieveryScope;
 import sopt.market.product.entity.Product;
 
-import java.util.List;
-
 public record DetailDataGetResponse(
-        List<DetailGetResponse> products
+        String name,
+        Integer price,
+        Integer discount,
+        String image,
+        Integer membersDiscount,
+        DelieveryScope delieveryType,
+        String seller,
+        String origin,
+        String packagingType,
+        String sellingUnit,
+        String weight,
+        String expiration,
+        Float brix,
+        String notification,
+        CategoryScope category,
+        String livestock,
+        String allergy
 ) {
-    public static DetailDataGetResponse from(final List<Product> products){
+    public static DetailDataGetResponse from(final Product product) {
         return new DetailDataGetResponse(
-                products.stream()
-                        .map(DetailGetResponse::from)
-                        .toList()
+                product.getName(),
+                product.getPrice(),
+                product.getDiscount(),
+                product.getImage(),
+                product.getMembersDiscount(),
+                product.getDelieveryType(),
+                product.getSeller(),
+                product.getOrigin(),
+                product.getPackagingType(),
+                product.getSellingUnit(),
+                product.getWeight(),
+                product.getExpiration(),
+                product.getBrix(),
+                product.getNotification(),
+                product.getCategory(),
+                product.getLivestock(),
+                product.getAllergy()
         );
-    }
-
-    public record DetailGetResponse(
-            String name,
-            Integer price,
-            Integer discount,
-            String image,
-            Integer membersDiscount,
-            DelieveryScope delieveryType,
-            String seller,
-            String origin,
-            String packagingType,
-            String sellingUnit,
-            String weight,
-            String expiration,
-            Float brix,
-            String notification,
-            CategoryScope category,
-            String livestock,
-            String allergy
-    ){
-        public static DetailGetResponse from(final Product product){
-            return new DetailGetResponse(
-                    product.getName(),
-                    product.getPrice(),
-                    product.getDiscount(),
-                    product.getImage(),
-                    product.getMembersDiscount(),
-                    product.getDelieveryType(),
-                    product.getSeller(),
-                    product.getOrigin(),
-                    product.getPackagingType(),
-                    product.getSellingUnit(),
-                    product.getWeight(),
-                    product.getExpiration(),
-                    product.getBrix(),
-                    product.getNotification(),
-                    product.getCategory(),
-                    product.getLivestock(),
-                    product.getAllergy()
-            );
-        }
     }
 }
