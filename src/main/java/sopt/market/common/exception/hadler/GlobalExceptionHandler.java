4888dc0ce9f6e033bbebd7b3859b1cd9f3f8df0d
sae2say <lollol0617@gmail.com>
2024-11-21 23:34:38 +0900

    [FIX] globalExceptionHandler의 반환 타입을 BaseResponse -> ErrorResponse로 변경

--- src/main/java/sopt/market/common/exception/hadler/GlobalExceptionHandler.java
+++ src/main/java/sopt/market/common/exception/hadler/GlobalExceptionHandler.java
@@ -19,28 +19,28 @@ public class GlobalExceptionHandler {
     private static final Logger logger = Logger.getLogger(GlobalExceptionHandler.class.getName());
 
     @ExceptionHandler(HttpServerErrorException.InternalServerError.class)
-    public ResponseEntity<BaseResponse> Exception(Exception exception) {
+    public ResponseEntity<ErrorResponse> Exception(Exception exception) {
         logger.log(Level.WARNING,exception.getMessage());
         ErrorCode errorCode = ErrorCode.SERVER_ERROR;
         return ResponseEntity.status(errorCode.getHttpStatus()).body(ErrorResponse.of(errorCode.getHttpStatus().value(), errorCode.getMessage()));
     }
 
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    public ResponseEntity<BaseResponse> handleMethodNotAllowed(HttpRequestMethodNotSupportedException exception) {
+    public ResponseEntity<ErrorResponse> handleMethodNotAllowed(HttpRequestMethodNotSupportedException exception) {
         logger.log(Level.WARNING,exception.getMessage());
         ErrorCode errorCode = ErrorCode.METHOD_NOT_ALLOWED;
         return ResponseEntity.status(errorCode.getHttpStatus()).body(ErrorResponse.of(errorCode.getHttpStatus().value(), errorCode.getMessage()));
     }
 
     @ExceptionHandler(NullPointerException.class)
-    public ResponseEntity<BaseResponse> handleNullPointer(NullPointerException exception) {
+    public ResponseEntity<ErrorResponse> handleNullPointer(NullPointerException exception) {
         logger.log(Level.WARNING,exception.getMessage());
         ErrorCode errorCode = ErrorCode.BAD_REQUEST;
         return ResponseEntity.status(errorCode.getHttpStatus()).body(ErrorResponse.of(errorCode.getHttpStatus().value(), errorCode.getMessage()));
     }
 
     @ExceptionHandler(ChangeSetPersister.NotFoundException.class)
-    public ResponseEntity<BaseResponse> handleNotFound(ChangeSetPersister.NotFoundException exception) {
+    public ResponseEntity<ErrorResponse> handleNotFound(ChangeSetPersister.NotFoundException exception) {
         logger.log(Level.WARNING,exception.getMessage());
         ErrorCode errorCode = ErrorCode.NOT_FOUND;
         return ResponseEntity.status(errorCode.getHttpStatus()).body(ErrorResponse.of(errorCode.getHttpStatus().value(), errorCode.getMessage()));
