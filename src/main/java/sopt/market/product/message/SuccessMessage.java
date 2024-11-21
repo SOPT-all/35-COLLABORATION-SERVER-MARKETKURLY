7af6e8e8114e945c21b5ebafb4bbb48f5aed3ffd
sae2say <lollol0617@gmail.com>
2024-11-21 20:20:42 +0900

    [REFAC] successMessage -> SuccessMessage

--- src/main/java/sopt/market/product/message/SuccessMessage.java
+++ src/main/java/sopt/market/product/message/SuccessMessage.java
@@ -1,10 +1,10 @@
 package sopt.market.product.message;
 
-public enum successMessage {
+public enum SuccessMessage {
     GET_MAINDATAS("메인 화면 데이터 불러오기 성공"),
     GET_DETAILDATAS("상품 상세 데이터 불러오기 성공");
 
-    successMessage(String message) {
+    SuccessMessage(String message) {
         this.message = message;
     }
 
